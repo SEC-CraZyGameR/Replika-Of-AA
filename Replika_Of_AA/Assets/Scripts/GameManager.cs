44d3ae4575d636e45695cbdbce596ace44549afd
Md Amanulla <41739520+SEC-CraZyGameR@users.noreply.github.com>
2018-07-26 21:12:20 +0600

    aa

---
+++ Replika_Of_AA/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+	public Animator animator;
+	private bool GameHasEnded = false;
+
+	public Rotator rotator;
+	public SpawnPin spawnpin;
+
+	public void GameEnd()
+	{
+		if (GameHasEnded)
+			return;
+		
+		rotator.enabled = false;
+		spawnpin.enabled = false;
+		GameHasEnded = true;
+		animator.SetTrigger ("EndGame");
+		Debug.Log ("GameEnd");
+	}
+
+	public void ResetLevel()
+	{
+
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+	}
+}
---
+++ Replika_Of_AA/Assets/Scripts/SpawnPin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPin : MonoBehaviour {
+
+	public GameObject pinPrefab;
+
+	void Update () {
+
+		if (Input.GetMouseButtonDown (0)) {
+			spawnPin ();
+		}
+		
+	}
+	void spawnPin()
+	{
+		Instantiate (pinPrefab, transform.position, transform.rotation);
+	}
+}
